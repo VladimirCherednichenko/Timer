16c551c414a5a2267def3c703c6ea85f9ae6849f
Vladimir <vladimir@MacBook-Pro-admin.local>
2017-07-03 19:07:03 +0300

    first commit

---
+++ Database.swift
@@ -0,0 +1,9 @@
+//
+//  Database.swift
+//  Timer
+//
+//  Created by Vladimir on 30.06.17.
+//  Copyright © 2017 Vladimir. All rights reserved.
+//
+
+import Foundation
--- Timer/JobsViewController.swift
+++ Timer/JobsViewController.swift
@@ -8,15 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class JobsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
+        self.view.backgroundColor = UIColor.white
         // Dispose of any resources that can be recreated.
     }
 

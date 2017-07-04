2a443b01962412308765715f07527a4a3dde358b
Vladimir <vladimir@MacBook-Pro-admin.local>
2017-07-04 16:19:54 +0300

    new version of TimerControl

---
+++ Timer/Fonts.swift
@@ -0,0 +1,15 @@
+//
+//  Fonts for Application.swift
+//  Timer
+//
+//  Created by Vladimir on 04.07.17.
+//  Copyright © 2017 Vladimir. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Fonts
+{
+    
+}

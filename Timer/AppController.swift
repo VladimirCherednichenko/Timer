fe901d1b1ac6a49ff5aaea7eefd303e0d7d70f3e
Vladimir Cherednichenko <Vladimir@MacBook-Pro-Vladimir.local>
2017-08-16 13:55:33 +0300

    database and pods

---
+++ Timer/AppController.swift
@@ -0,0 +1,9 @@
+//
+//  AppControler.swift
+//  Timer
+//
+//  Created by Vladimir Cherednichenko on 16.08.17.
+//  Copyright © 2017 Vladimir Cherednichenko. All rights reserved.
+//
+
+import Foundation
---
+++ Timer/DataBase.swift
@@ -0,0 +1,9 @@
+//
+//  DataBase.swift
+//  Timer
+//
+//  Created by Vladimir Cherednichenko on 16.08.17.
+//  Copyright © 2017 Vladimir Cherednichenko. All rights reserved.
+//
+
+import Foundation

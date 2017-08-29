570f2148d4f2ac1a3cecd383a4a8bbbd92056999
VladimirCherednichenko <cherid95@gmail.com>
2017-08-29 13:34:52 +0300

    TimeCircle without functionality

---
+++ CircleView.swift
@@ -0,0 +1,9 @@
+//
+//  CircleView.swift
+//  Timer
+//
+//  Created by Vladimir Cherednichenko on 25.08.17.
+//  Copyright © 2017 Vladimir Cherednichenko. All rights reserved.
+//
+
+import Foundation
---
+++ EmptyViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EmptyViewController.swift
+//  Timer
+//
+//  Created by Vladimir Cherednichenko on 25.08.17.
+//  Copyright © 2017 Vladimir Cherednichenko. All rights reserved.
+//
+
+import Foundation
